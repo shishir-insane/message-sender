4726c19da7d0eeb56895d580740a114e48157235
Shishir Kumar <shishir.insane@gmail.com>
2018-02-18 23:25:41 +0000

    Initial Base Commit

---
+++ src/main/java/com/sk/ubs/MessageSenderApplication.java
@@ -0,0 +1,12 @@
+package com.sk.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSenderApplication.class, args);
+	}
+}
